c8167cda1a4ed91b8fa8a4f06e706d71b0824930
ScottWaring <scott.waring@gmail.com>
2018-10-27 14:34:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+
+function printBadges(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let printVar = i + 1;
+    console.log('Welcome ' + arr[i] + '! You are employee #'+ printVar +'.');
+  }
+  return arr;
+};
+
+
+function tailsNeverFails() {
+  var coin;
+  let tailsCounter = 0;
+  while (coin === "Tails" || coin === undefined) {
+    let num = Math.random();
+    if (num >= .5) {
+      tailsCounter += 1;
+      coin = "Tails"
+    } else if (num < .5) {
+      return 'You got '+tailsCounter+' tails in a row!';
+    }
+  }
+
+}
