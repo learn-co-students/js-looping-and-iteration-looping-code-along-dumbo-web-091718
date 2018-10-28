afb73a9a9ba3c434b7705dce55db8d0122afba36
github username <github email address>
2018-10-28 13:45:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solutions in this file
+//for ([initialization]; [condition]; [iteration]) {[loop body]}
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+// Wrapp Gifts
+ const gifts = ['teddy bear', 'drone', 'doll'];
+
+ function wrappGifts(gifts){
+   for(let i = 0; i < gifts.length; i++){
+     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+   };
+   return gifts
+ }
+ wrappGifts(gifts)
+
+//Prints Badges & Welcomes with employee position
+function printBadges(badges){
+ for(let i = 0; i < badges.length; i++ ){
+   console.log(`Welcome ${badges[i]}! You are employee #${i+1}.`);
+ };
+ return badges
+}
+printBadges(['Ada', 'Brendan', 'Ali']);
+
+// invokes Math.random() to simulate coin flips
+
+
+
+function tailsNeverFails() {
+
+  let flip = 'tails';
+  let wins = 0;
+  while (flip === 'tails') {
+    if (Math.random() >= 0.5) {
+      wins++;
+    } else {
+      flip = 'heads'
+      return `You got ${wins} tails in a row!`;
+    }
+  }
+}
+// function tailsNeverFails() {
+//   let flip = 'tails';
+//   let wins = 0;
+//   while (flip === 'tails') {
+//     if (Math.random() >= 0.5) {
+//       wins++;
+//     } else {
+//       flip = 'heads'
+//       return `You got ${wins} tails in a row!`;
+//     }
+//   }
+// }
