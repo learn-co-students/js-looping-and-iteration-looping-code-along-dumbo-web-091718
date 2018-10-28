febee32b8190744d939e744f15523fea450d5580
shemarjoseph <shemarjoseph@yahoo.com>
2018-10-28 16:16:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (var i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`);
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  wins = 0
+  while (Math.random() >= 0.5) {
+    wins += 1;
+  }
+  return `You got ${wins} tails in a row!`;
+}
